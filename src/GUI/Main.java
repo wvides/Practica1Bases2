61ffbecf85b5f9b7a9fe57deb10a21ae13daf1c5
Omar Vides <omarvides@gmail.com>
2012-06-11 21:39:33 -0600

    Creacion de Package de interfaz grafica GUI, y cambio de lugar de la clase Main.java para el package GUI

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -2,8 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package Mapeo;
+package GUI;
 
+import Mapeo.Modelo;
 import java.util.ArrayList;
 
 /**
